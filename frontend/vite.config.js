dc52aa135b5dbf11ff57f60d19437407d2a35aa8
nimeshaviduranga <49368109+nimeshaviduranga@users.noreply.github.com>
2025-05-22 10:18:45 +0530

    Initial Project setup using vite

---
+++ frontend/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()], 4server: {
+  port: 3000, // Set the development server port
+  proxy: {
+    // Proxy API requests to the backend server (will be used later)
+    '/api': {
+      target: 'http://localhost:8080',
+      changeOrigin: true,
+      secure: false,
+    }
+  }
+}
+})
